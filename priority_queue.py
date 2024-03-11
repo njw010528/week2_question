3f5f426f9399fdfe3fec401a25bff1ec530244d3
njw010528 <162092622+njw010528@users.noreply.github.com>
2024-03-11 17:09:07 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -3,18 +3,18 @@ class PriorityQueue:
     self.heap = []
 
   def push(self, item, priority):
-    entry = (_, item)
+    entry = (priority, item)
     self.heap.append(entry)
     self._sift_up(len(self.heap) - 1)
 
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      priority, item = self.heap.pop()
+      _, item = self.heap.pop()
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
-      priority, item = self.heap.pop()
+      _, item = self.heap.pop()
       return item
     else:
       return None
